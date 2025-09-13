7d74b23da3d539cbaf603ea6e32735b48bf5d6c7
casizzy <scr38919@lasallebajio.edu.mx>
2025-09-13 12:16:04 -0600

    crear columnas

--- app/src/main/java/com/stellacr/scasillascoinsphere/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/stellacr/scasillascoinsphere/ui/theme/HomeScreen.kt
@@ -1,11 +1,21 @@
 package com.stellacr.scasillascoinsphere.ui.theme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.stellacr.scasillascoinsphere.ui.theme.Surface
 
 val Background = Color(0xFF0B1020)
@@ -25,10 +35,36 @@ fun AppCoinSphere(){
     }
 }
 
+@Composable
 fun HomeScreen(){
     val listaCrypto = listOf(
         CryptoM("Bitcoin", "$34353", "sad")
     )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Text(
+            text = "CoinSphere",
+            color = TextDim,
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        LazyColumn {
+            itemsIndexed(listaCrypto) { indice, crypto ->
+                Spacer(modifier = Modifier.height(8.dp))
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
